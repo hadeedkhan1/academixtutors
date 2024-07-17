2fca3f781dd34b37d3cf0893722d93e8a6fb3e31
Hadeed Khan <128728518+hadeedkhan1@users.noreply.github.com>
2024-07-17 16:37:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    var password = document.getElementById("password").value;
+
+    if (password === "edwin34") {
+        window.location.href = "33946ed6746.html"; // Redirect
+    }
+    else if (password === "lila357") {
+        window.location.href = "5452li8145.html"; // Redirect
+    }
+    else if (password === "autumn1966") {
+        window.location.href = "288866au0amgc631966.html"; // Redirect
+    }
+    else if (password === "carter0366") {
+        window.location.href = "45670366902ca528.html"; // Redirect
+    }
+    else if (password === "carter0366") {
+        window.location.href = "45670366902ca528.html"; // Redirect
+    }
+    else {
+        document.getElementById("message").innerHTML = "Incorrect password. Please try again.";
+    }
+});
