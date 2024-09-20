5b8b15d80604e915dc83371e992b47cd4fbdf93c
Hadeed Khan <128728518+hadeedkhan1@users.noreply.github.com>
2024-09-19 20:48:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     else if (password === "lila357") {
         window.location.href = "5452li8145.html"; // Redirect
     }
-     else if (password === "marcus64") {
+    else if (password === "marcus64") {
         window.location.href = "ma82916465.html"; // Redirect
     }    
     else if (password === "autumn1966") {
@@ -18,8 +18,8 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     else if (password === "carter0366") {
         window.location.href = "45670366902ca528.html"; // Redirect
     }
-    else if (password === "carter0366") {
-        window.location.href = "45670366902ca528.html"; // Redirect
+    else if (password === "marcus64") {
+        window.location.href = "ma82916465.html"; // Redirect
     }
     else {
         document.getElementById("message").innerHTML = "Incorrect password. Please try again.";
